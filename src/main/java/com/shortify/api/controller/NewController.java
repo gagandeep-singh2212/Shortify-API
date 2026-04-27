438921e417808fb3e1825ca302c46ced724caf8d
Gagandeep Singh <2001gagandeepsinghk@gmail.com>
2026-04-27 16:39:18 +0530

    Create NewController.java

---
+++ src/main/java/com/shortify/api/controller/NewController.java
@@ -0,0 +1,18 @@
+package com.shortify.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController("/new")
+public class NewController {
+    @GetMapping()
+    public ResponseEntity<Void> redirect() {
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
